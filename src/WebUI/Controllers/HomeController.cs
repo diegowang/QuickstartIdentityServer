326e500bf7ec25f99af6ca226dc48409f6b00ac4
hwang <hwang@BC1HWANG>
2017-02-28 14:52:15 -0500

    Moved helpers folder to root and Added Security Headers for Home controller.

--- src/WebUI/Controllers/HomeController.cs
+++ src/WebUI/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.wwwroot.Helpers;
 
 namespace WebUI.Controllers
 {
+    [SecurityHeaders]
     public class HomeController : Controller
     {
         public IActionResult Index()
