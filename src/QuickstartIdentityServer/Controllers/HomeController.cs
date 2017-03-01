050c7dc27360910fc8ebc9386ad0ba005f7786ce
hwang <hwang@BC1HWANG>
2017-03-01 15:29:26 -0500

    Added Error view model to home controller.

--- src/QuickstartIdentityServer/Controllers/HomeController.cs
+++ src/QuickstartIdentityServer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using IdentityServer4.Services;
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Mvc;
+using QuickstartIdentityServer.models.Home;
 using WebUI.wwwroot.Helpers;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,6 +22,23 @@ namespace QuickstartIdentityServer.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Shows the error page
+        /// </summary>
+        public async Task<IActionResult> Error(string errorId)
+        {
+            var vm = new ErrorViewModel();
+
+            // retrieve error details from identityserver
+            var message = await _interaction.GetErrorContextAsync(errorId);
+            if (message != null)
+            {
+                vm.Error = message;
+            }
+
+            return View("Error", vm);
         }
     }
 }
