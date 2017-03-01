9e5c293e4ea2b06ead0b2e6fc77ca819b460e05b
hwang <hwang@BC1HWANG>
2017-03-01 15:18:10 -0500

    Added MVC packages and set up the home controller to run.

---
+++ src/QuickstartIdentityServer/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.wwwroot.Helpers;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuickstartIdentityServer.Controllers
+{
+    [SecurityHeaders]
+    public class HomeController : Controller
+    {
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public HomeController(IIdentityServerInteractionService interaction)
+        {
+            _interaction = interaction;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/QuickstartIdentityServer/Startup.cs
+++ src/QuickstartIdentityServer/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +11,8 @@ namespace QuickstartIdentityServer
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             // configure identity server with in-memory stores, keys, clients and scopes
             services.AddIdentityServer()
                 .AddTemporarySigningCredential()
@@ -36,7 +33,10 @@ namespace QuickstartIdentityServer
             }
 
             app.UseIdentityServer();
-            
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
         }
     }
 }
