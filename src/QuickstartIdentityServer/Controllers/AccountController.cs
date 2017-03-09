c068a3b106c08a0731680c8f4e4592437f8d40c5
hwang <hwang@BC1HWANG>
2017-03-09 10:26:48 -0500

    Added login post process

--- src/QuickstartIdentityServer/Controllers/AccountController.cs
+++ src/QuickstartIdentityServer/Controllers/AccountController.cs
@@ -64,6 +64,68 @@ namespace QuickstartIdentityServer.Controllers
         }
 
         /// <summary>
+        /// Handle postback from username/password login
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // validate username/password against in-memory store
+                if (_users.ValidateCredentials(model.Username, model.Password))
+                {
+                    AuthenticationProperties props = null;
+                    // only set explicit expiration here if persistent. 
+                    // otherwise we reply upon expiration configured in cookie middleware.
+                    if (AccountOptions.AllowRememberLogin && model.RememberLogin)
+                    {
+                        props = new AuthenticationProperties
+                        {
+                            IsPersistent = true,
+                            ExpiresUtc = DateTimeOffset.UtcNow.Add(AccountOptions.RememberMeLoginDuration)
+                        };
+                    }
+
+                    // issue authentication cookie with subject ID and username
+                    var user = _users.FindByUsername(model.Username);
+                    await HttpContext.Authentication.SignInAsync(user.SubjectId, user.Username, props);
+
+                    // make sure the returnUrl is still valid, and if yes - redirect back to authorize endpoint
+                    if (_interaction.IsValidReturnUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+
+                    return Redirect("~/");
+                }
+
+                ModelState.AddModelError("",AccountOptions.InvalidCredentialsErrorMessage);
+            }
+
+            var vm = await _account.BuildLoginViewModelAsync(model);
+            return View(vm);
+        }
+
+
+        /// <summary>
+        /// Show logout page
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            var vm = await _account.BuildLogoutViewModelAsync(logoutId);
+
+            if (vm.ShowLogoutPrompt == false)
+            {
+                // no need to show prompt
+                return await Logout(vm);
+            }
+
+            return View(vm);
+        }
+
+        /// <summary>
         /// initiate roundtrip to external authentication provider
         /// </summary>
         [HttpGet]
