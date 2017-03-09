7682ebafa07484ac948c1b2f958525b4a47fbcfe
hwang <hwang@BC1HWANG>
2017-03-09 16:26:37 -0500

    Added ConsentController first action to show the consent screen.

---
+++ src/QuickstartIdentityServer/Controllers/ConsentController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuickstartIdentityServer.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuickstartIdentityServer.Controllers
+{
+    public class ConsentController : Controller
+    {
+        private readonly ConsentService _consent;
+
+        public ConsentController(
+            IIdentityServerInteractionService interaction,
+            IClientStore clientStore,
+            IResourceStore resourceStore,
+            ILogger<ConsentController> logger)
+        {
+            _consent = new ConsentService(interaction,clientStore,resourceStore,logger);
+        }
+
+        /// <summary>
+        /// Shows the consent screen
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Index(string returnUrl)
+        {
+            var vm = await _consent.BuildViewModelAsync(returnUrl);
+            if (vm != null)
+            {
+                return View("Index", vm);
+            }
+
+            return View("Error");
+        }
+    }
+}
