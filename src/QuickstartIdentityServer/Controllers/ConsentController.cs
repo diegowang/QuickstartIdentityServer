928bc6d3d67c1352ba2ec8ee52afd51c79fc6972
hwang <hwang@BC1HWANG>
2017-03-10 10:42:43 -0500

    Finished ConsentController

--- src/QuickstartIdentityServer/Controllers/ConsentController.cs
+++ src/QuickstartIdentityServer/Controllers/ConsentController.cs
@@ -3,6 +3,7 @@ using IdentityServer4.Services;
 using IdentityServer4.Stores;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using QuickstartIdentityServer.models.Consent;
 using QuickstartIdentityServer.Services;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -34,6 +35,33 @@ namespace QuickstartIdentityServer.Controllers
             if (vm != null)
             {
                 return View("Index", vm);
+            }
+
+            return View("Error");
+        }
+
+        /// <summary>
+        /// Handles the consent screen postback
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(ConsentInputModel model)
+        {
+            var result = await _consent.ProcessConsent(model);
+
+            if (result.IsRedirect)
+            {
+                return Redirect(result.RedirectUri);
+            }
+
+            if (result.HasValidationError)
+            {
+                ModelState.AddModelError("", result.ValidationError);
+            }
+
+            if (result.ShowView)
+            {
+                return View("Index", result.ViewModel);
             }
 
             return View("Error");
